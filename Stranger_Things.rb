5bf690389d93d7a6ffbd2264f9e240de513fdeb8
Matt808 <54596379+Matt808@users.noreply.github.com>
2020-01-17 14:00:19 -0800

    Add files via upload

---
+++ Stranger_Things.rb
@@ -0,0 +1,21 @@
+# Welcome to Sonic Pi v3.1
+
+use_synth :hoover
+live_loop :high_notes do
+  play :C4
+  sleep 0.25
+  play :E4
+  sleep 0.25
+  play :G4
+  sleep 0.25
+  play :B4
+  sleep 0.25
+  play :C5
+  sleep 0.25
+  play :B4
+  sleep 0.25
+  play :G4
+  sleep 0.25
+  play :E4
+  sleep 0.25
+end
